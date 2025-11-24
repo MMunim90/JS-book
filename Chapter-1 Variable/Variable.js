3fcde4934c2b3f4bdc2ceeb943f08540f02bfbda
MMunim <mmunim9.9.01@gmail.com>
2025-11-24 18:59:34 +0600

    declare some variable and console it

--- Chapter-1 Variable/Variable.js
+++ Chapter-1 Variable/Variable.js
@@ -1,20 +1,13 @@
 const birth_year = 2001;
-console.log(birth_year);
 
 const number_of_bro_sis = 2;
-console.log(number_of_bro_sis);
 
 const students = 14;
-console.log(students);
 
 const marks = 12;
-console.log(marks);
 
 const rooms = 3;
-console.log(rooms);
 
 const study_hour = 6;
-console.log(study_hour);
 
 const total = 38;
-console.log(total);
---
+++ Chapter-1 Variable/console.js
@@ -0,0 +1,21 @@
+const age = 24;
+console.log(age);
+
+const booksToRead = 1;
+console.log(booksToRead);
+
+let wakeUpTime = 9;
+wakeUpTime = 7;
+console.log(wakeUpTime);
+
+const wastedTime = 1;
+console.log(wastedTime);
+
+const borrow = 0;
+console.log(borrow);
+
+const goals = 1;
+console.log(goals);
+
+const pillow = 2;
+console.log(pillow);
