435faa95103d107bd1e9d28e8404a4dcf7639566
MMunim <mmunim9.9.01@gmail.com>
2025-11-24 19:10:54 +0600

    explore string

---
+++ Chapter-1 Variable/string.js
@@ -0,0 +1,20 @@
+const name = "munim";
+console.log(name);
+
+const address = "mohammadpur, dhaka";
+console.log(address);
+
+const varsity = "daffodil";
+console.log(varsity);
+
+const fan = "sabnur";
+console.log(fan);
+
+const movieOfTheYear = "o re piya";
+console.log(movieOfTheYear);
+
+const hobby = "gaming";
+console.log(hobby);
+
+const color = "gray"
+console.log(color);
