3708f4709c00784fda0120de793f3314f9d6ed05
MMunim <mmunim9.9.01@gmail.com>
2025-11-30 19:07:18 +0600

    bool done

---
+++ Chapter-1 Variable/bool.js
@@ -0,0 +1,24 @@
+let is_happy = false;
+console.log(is_happy);
+
+let is_receive_message = true;
+console.log(is_receive_message);
+
+const celebrating = true;
+console.log(celebrating);
+
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+console.log(isHappy);
+console.log(hasMoney);
+console.log(isHungry);
+
+const isStudyDone = false;
+console.log(isStudyDone);
+
+const isPhoneCharged = true;
+console.log(isPhoneCharged);
+
+const isDinnerDone = false;
+console.log(isDinnerDone);
---
+++ Chapter-1 Variable/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+let is_happy = false;
+// console.log(is_happy);
+
+// let is_receive_message = true;
+// console.log(is_receive_message);
+
+// const celebrating = true;
+// console.log(celebrating);
